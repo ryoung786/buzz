81368e2658617a4b9fecac6811d96dd3ed342a4f
Ryan Young <ryoung786@gmail.com>
2011-06-16 00:06:39 -0400

    yeah, don't lose these shitty changes

---
+++ game.js
@@ -0,0 +1,54 @@
+gs = require('./game_states');
+
+Game = function(){
+	this.state = gs.INIT;
+    this.players = {}; // session_id -> player object
+    this.question;
+    this.db = new mongodb.Server("127.0.0.1", 27017, {});
+};
+
+Game.prototype = {
+
+    addNewPlayer : function(session_id) {
+        this.players[session_id] = 1;
+        if (players.length == 1) {
+            // ask a question?  sure why not
+            this.askQuestion();
+        }
+        return this.players;
+    },
+
+    askQuestion : function() {
+        new mongodb.Db('buzz', db, {}).open(function (err, client) {
+            if (err) throw err;
+            var coll = new mongodb.Collection(client, 'questions');
+            coll.find().toArray(function (err, docs) {
+                var i = Math.floor(Math.random()*docs.length);
+                // raise new question event for docs[i]
+            });
+        });
+    }
+
+	playerAnswered : function(data, player_id) {
+		if (this.state != WAITING_FOR_ANSWER) {
+			throw new WrongStateException();
+		} else if (this.current_buzz.player_id != player_id) {
+			throw new WrongPlayerAnsweredBuzzException();
+		}
+	},
+
+    // return true if given_answer is in answer (if array)
+    //             or if given_answer == answer
+    // case insensitive
+    isCorrect: function(given_answer, answer) {
+        given_answer = given_answer.toLowerCase();
+        if (answer instanceof Array) {
+            if (ans.indexOf(given_answer) >= 0) { return true; }
+            var x = parseInt(given_answer);
+            return !isNaN(x) && ans.indexOf(x) >= 0;
+        }
+        return given_answer == current_question.answer;
+    }
+}
+
+exports.Game = Game;
--- server.js
+++ server.js
@@ -2,90 +2,95 @@ var http = require('http');
 var io = require('socket.io');
 var game_states = require('./game_states');
 var mongodb = require('mongodb');
+require('./game');
+
 
 var server = http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<h1>Hello world</h1>');
 });
 server.listen(8080);
-
 var socket = io.listen(server);
 
 
 var game_state = game_states.INIT;
-
 var current_question = null;
-
 var current_buzzer_client_id = null;
-
 var answer_tries = 0;
-
 var buzz_timeout;
-
-var questions = [ {question: '2+2=?', answer: '4'},
-				  {question: 'first US president?', answer: 'Washington'},
-				  {question: '3*3=?', answer: '9'} ];
-
-var clients = {};
+var games = {};
 
 socket.on('connection', function(client){
-	clients[''+client.sessionId] = 1;
 
-    var data = { acton: 'joined', id: client.sessionId, all: Object.keys(clients) };
-    socket.broadcast(data);
-
-	if (Object.keys(clients).length == 1) {
-		askQuestion();
-	} else {
-		client.send({action: 'new_question', question: current_question.question});
-	}
+    //TODO move into Game
+    // if (clients.length == 1) {
+    //     askQuestion();
+    // } else {
+    //     client.send({action: 'new_question', question: current_question.question});
+    // }
 
     client.on('message', function(message){
         console.log("got message from " + client.sessionId + ", " +
                     JSON.stringify(message));
         switch (message.action) {
-        case 'answer':
-            handleAnswer(message, client);
-            break;
-        case 'buzz':
-			handleBuzz(client);
-            break;
-        default:
-            break;
+          case 'join':
+            handleJoin(message, client); break;
+          case 'answer':
+            handleAnswer(message, client); break;
+          case 'buzz':
+            handleBuzz(client); break;
+          default: break;
         }
         return true;
     });
 
     client.on('disconnect', function(){
-		delete(clients[''+client.sessionId]);
-		var data = { action: 'left', id: client.sessionId, all: Object.keys(clients) };
-		socket.broadcast(data);
+        //TODO move into Game
+        // delete(clients[''+client.sessionId]);
+        // var data = { action: 'left', id: client.sessionId, all: Object.keys(clients) };
+        // socket.broadcast(data);
         return true;
     });
 });
 
-function validateAnswer(given) {
-    return given == current_question.answer;
-}
-
 function connectdb(callback) {
     var server = new mongodb.Server("127.0.0.1", 27017, {});
     new mongodb.Db('buzz', server, {}).open(function (error, client) {
-		if (error) throw error;
-		callback(client);
-	});
+        if (error) throw error;
+        callback(client);
+    });
+}
+
+function handleJoin(data, client) {
+    // data:  { action: 'join', game_id: 1234 }
+
+    // retrieve the game from the game cache
+    var game = games[data.game_id];
+
+    // if no game exists, create a new game, add it to the cache
+    if (!game) {
+        game = new Game();
+        games[data.game_id] = game;
+    }
+
+    // tell the game instance we have a new player
+    var all_players = game.addNewPlayer(client.sessionId);
+
+    // tell all the players who joined
+    var data = { acton: 'joined', id: client.sessionId, all: all_players };
+    socket.broadcast(data);
 }
 
 function handleAnswer(data, client) {
     if (game_state != game_states.WAITING_FOR_ANSWER) {
         client.send({action: 'error',
                      error: "you can't answer before buzzing in"});
-		console.log("you can't answer before buzzing in; in state " + game_state);
+        console.log("you can't answer before buzzing in; in state " + game_state);
         return;
     } else if (client.sessionId != current_buzzer_client_id) {
         client.send({action: 'error',
                      error: "you can't answer when you haven't buzzed in"});
-		console.log("you can't answer when you haven't buzzed in; state " + game_state);
+        console.log("you can't answer when you haven't buzzed in; state " + game_state);
         return;
     }
     clearTimeout(buzz_timeout);
@@ -95,11 +100,11 @@ function handleAnswer(data, client) {
     socket.broadcast(response);
 
     if (response.correct || ++answer_tries > 2) {
-		console.log(data.value + " was correct. asking a new question");
+        console.log(data.value + " was correct. asking a new question");
         answer_tries = 0;
         askQuestion();
     } else {
-		console.log(data.value + " was incorrect. Looking for " + current_question.answer);
+        console.log(data.value + " was incorrect. Looking for " + current_question.answer);
         game_state = game_states.ASKING_QUESTION;
     }
 }
@@ -108,48 +113,48 @@ function handleBuzz(client) {
     if (game_state != game_states.ASKING_QUESTION) {
         client.send({action: 'error',
                      error: "you can't buzz when a question isn't being asked"});
-		console.log("you can't buzz when in state " + game_state);
+        console.log("you can't buzz when in state " + game_state);
         return;
     }
     current_buzzer_client_id = client.sessionId;
-	// inform the client that they won the buzz and should now submit an answer
-	var response = { action: 'you_buzzed' };
-	client.send(response);
-	// broadcast to everyone except the one who buzzed in
-	response = { action: 'someone_buzzed', id: client.sessionId };
-	socket.broadcast(response, client.sessionId);
+    // inform the client that they won the buzz and should now submit an answer
+    var response = { action: 'you_buzzed' };
+    client.send(response);
+    // broadcast to everyone except the one who buzzed in
+    response = { action: 'someone_buzzed', id: client.sessionId };
+    socket.broadcast(response, client.sessionId);
 
-	game_state = game_states.WAITING_FOR_ANSWER;
+    game_state = game_states.WAITING_FOR_ANSWER;
 
-	// they now have 5 seconds to submit an answer
-	startCountdown();
+    // they now have 5 seconds to submit an answer
+    startCountdown();
 }
 
 function startCountdown() { // make a class for this?
     buzz_timeout = setTimeout(function() {
         socket.broadcast({ action: 'time_up' });
-		if (++answer_tries > 2) {
-			answer_tries = 0;
-			askQuestion();
-		} else {
-			game_state = game_states.ASKING_QUESTION;
-		}
+        if (++answer_tries > 2) {
+            answer_tries = 0;
+            askQuestion();
+        } else {
+            game_state = game_states.ASKING_QUESTION;
+        }
     }, 5000);
 }
 
 function askQuestion() {
-	game_state = game_states.ASKING_QUESTION;
-	
+    game_state = game_states.ASKING_QUESTION;
+
     var server = new mongodb.Server("127.0.0.1", 27017, {});
     new mongodb.Db('buzz', server, {}).open(function (error, client) {
         if (error) throw error;
         var collection = new mongodb.Collection(client, 'questions');
-		collection.count({}, function(err, count) {
-			var offset = Math.floor(Math.random()*count)
-			collection.find({}, {limit: 1, skip: offset}).toArray(function(err, docs) {
-				current_question = docs[0];
-				socket.broadcast({action: 'new_question', question: current_question.question});
-			});
-		});
+        collection.count({}, function(err, count) {
+            var offset = Math.floor(Math.random()*count)
+            collection.find({}, {limit: 1, skip: offset}).toArray(function(err, docs) {
+                current_question = docs[0];
+                socket.broadcast({action: 'new_question', question: current_question.question});
+            });
+        });
     });
 }
